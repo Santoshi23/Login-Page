83f031ff38144cafa7fafaaecc0643bc9982f1e7
Santoshi23 <santoshi09315@gmail.com>
2023-10-06 15:35:01 +0530

    initial

---
+++ c/ifelse.c
@@ -0,0 +1,595 @@
+#include <stdio.h>
+
+// How to take string.
+
+// int main() {
+//     char string[] = "1234abc";
+//     printf("%s\n", string);
+//     return 0;
+// }
+
+// how to take interger array.
+
+    // int main() {
+    //     int array[] = {1,2,3,4,5,6};
+    //     for (int i=0; i<6; i++)
+    //         printf("%d",array[i]);
+    //     printf("\n");
+    //     return 0;
+    // }
+
+// int main() {
+//   int t;
+//   scanf("%d",&t);
+//   char X[max_len];
+//   for (int i=0;i<t;i++) {
+//     scanf("%s",S);
+//     int len=strlen(S);
+//     char X[max_len*2];
+//     int j;
+//     for (j=0;j<len,j++) {
+//       X[j]=S[j];
+//     }
+//     int k;
+//     for(k=0;k<len;k++) {
+//       X[j+k]=S[k];
+//     X[j+k]='\0';
+//     printf("%s\n",X);
+//   }
+//   return 0;
+// }
+
+// 1.Write a python program to find maximum between two numbers.
+
+// int main() {
+//     int a,b;
+//     printf("enter a and b");
+//     scanf("%d %d",&a,&b);
+//     if (a>b) {
+//         printf("a is greater");
+//     } else {
+//         printf("b is greater");
+//     }
+//     return 0;
+// }
+
+// 2. Write a python program to find maximum between three numbers.
+
+// int main() {
+//     int a,b,c;
+//     printf("enter a b c");
+//     scanf("%d %d %d",&a,&b,&c);
+//     if (a>b && a>c) {
+//         printf("a");
+//     } else if (b>a && b>c) {
+//         printf("b");
+//     } else {
+//         printf("c");
+//     }
+// }
+
+// 3. Write a python program to check whether a number is negative, positive or zero.
+
+// int main() {
+//     int a;
+//     printf("enter char");
+//     scanf("%i",&a);
+//     if (a>0) {
+//         printf("positive");
+//     } else if (a<0) {
+//         printf("negative");
+//     } else {
+//         printf("zero");
+//     }
+//     return 0;
+// }
+
+// 4. Write a python program to check whether a number is divisible by 5 and 11 or not.
+
+// int main() {
+//     int a;
+//     printf("enter a");
+//     scanf("%d",&a);
+//     if (a%5==0 && a%11==0) {
+//         printf("yes");
+//     } else {
+//         printf("no");
+//     }
+//     return 0;
+// }
+
+// 5. Write a python program to check whether a number is even or odd.
+
+// int main() {
+//     int a;
+//     printf("enter a");
+//     scanf("%i",&a);
+//     if (a%2==0) {
+//         printf("even");
+//     } else {
+//         printf("odd");
+//     }
+//     return 0;
+// }
+
+// 6. Write a python program to check whether a year is leap year or not.
+
+// int main() {
+//     int y;
+//     printf("enter y");
+//     scanf("%i",&y);
+//     if (y%4==0 && y%100!=0) {
+//         printf("leap");
+//     } else if (y%400==0 && y%100==0){
+//         printf("leap");
+//     } else {
+//         printf("not leap");
+//     }
+//     return 0;
+// }
+
+// 7. Write a python program to check whether a character is an alphabet or not.
+
+// int main() {
+//     char a;
+//     printf("enter a");
+//     scanf("%c",&a);
+//     if (a>='A' && a<='Z') {
+//         printf("alphabet");
+//     } else if (a>='a' && a<='z'){
+//         printf("alphabet");
+//     } else {
+//         printf("not alphabet");
+//     }
+//     return 0;
+// }
+
+// 8. Write a python program to input any alphabet and check whether it is vowel or consonant.
+
+// int main() {
+//     char a;
+//     printf("enter a");
+//     scanf("%c",&a);
+//     if (a=='a' || a=='e' || a=='i' || a=='o' || a=='u') {
+//         printf("vowel");
+//     } else {
+//         printf("constonant");
+//     }
+//     return 0;
+// }
+
+// 9. Write a python program to input any character and check whether it is alphabet, 
+// digit or special character.
+
+// int main() {
+//     char a;
+//     printf("enter a");
+//     scanf("%c",&a);
+//     if (a>='a' && a<='z') {
+//         printf("alphabet");
+//     } else if (a>='0' && a<='9') {
+//         printf("number");
+//     } else {
+//         printf("speacial character");
+//     }
+//     return 0;
+// }
+
+// 10. Write a python program to check whether a character is uppercase or lowercase alphabet.
+
+// int main() {
+//     char a;
+//     printf("enter a");
+//     scanf("%c",&a);
+//     if (a>='a' && a<='z') {
+//         printf("lowercase");
+//     } else {
+//         printf("upper");
+//     }
+//     return 0;
+// }
+
+// 11. Write a python program to input week number and print week day.
+
+// I am skip this question
+
+// 12. Write a python program to input the month number and print the number of days 
+// in that month.
+
+// I am skip this question.
+
+// 13. Write a python program to count the total number of notes in a given amount.
+
+// int main() {
+//     int amount;
+//     printf("enter amount");
+//     scanf("%i",&amount);
+
+// }
+
+// 14. Write a python program to input angles of a triangle and check whether
+//  triangle is valid or not.
+
+// int main() {
+//     int a,b,c,sum;
+//     printf("enter angles a b c");
+//     scanf("%i %i %i",&a,&b,&c);
+//     sum=a+b+c;
+//     if (sum==180) {
+//         printf("valid triangle");
+//     } else {
+//         printf("not valid triangle");
+//     }
+//     return 0;
+// }
+
+// 15. Write a python program to input all sides of a triangle and check whether 
+// the triangle is valid or not.
+
+// I am skip this question.
+
+// 16. Write a python program to check whether the triangle is equilateral, 
+// isosceles or scalene triangle.
+
+// I am skip this question.
+
+// 17. Write a python program to calculate profit or loss.
+
+// I am skip this questions.
+
+// 18. Write a python program to input marks of five subjects Physics, Chemistry, 
+// Biology, Mathematics and Computer. Calculate percentage and grade according to following:
+// Percentage >= 90% : Grade A
+// Percentage >= 80% : Grade B
+// Percentage >= 70% : Grade C
+// Percentage >= 60% : Grade D
+// Percentage >= 40% : Grade E
+// Percentage < 40% : Grade F
+
+// I am skip this questions.
+
+// 19. Write a python program to input basic salary of an employee and 
+// calculate its Gross salary according to following:
+// Basic Salary <= 10000 : HRA = 20%, DA = 80%
+// Basic Salary <= 20000 : HRA = 25%, DA = 90%
+// Basic Salary > 20000 : HRA = 30%, DA = 95%
+
+// I am skip this questions.
+
+// 20. Write a python program to input electricity unit charges and calculate 
+// total electricity bill according to the given condition:
+// For first 50 units Rs. 0.50/unit
+// For next 100 units Rs. 0.75/unit
+// For next 100 units Rs. 1.20/unit
+// For unit above 250 Rs. 1.50/unit
+// An additional surcharge of 20% is added to the bill
+
+// I am skip this questions.
+
+// 21. A shop will give a discount of 10% if the cost of the purchased quantity is 
+// more than 1000. Ask the user for quantity, Suppose, one unit will cost 100. 
+// Judge and print total cost for user.
+
+// I am skip this questions.
+
+// 22. A company decided to give a bonus of 5% to an employee if his/her year of service 
+// is more than 5 years. Ask users for their salary and year of service and print the net 
+// bonus amount.
+
+// I am skip this questions.
+
+// 23. A school has following rules for grading system:
+// a. Below 25 - F
+// b. 25 to 45 - E
+// c. 45 to 50 - D
+// d. 50 to 60 - C
+// e. 60 to 80 - B
+// f. Above 80 - A
+// Ask user to enter marks and print the corresponding grade.
+
+// I am skip this questions.
+
+// 24. Take the age of 3 people by user and determine oldest and youngest among them.
+
+// int main() {
+//     int a,b,c;
+//     printf("enter age");
+//     scanf("%i %i %i",&a,&b,&c);
+//     if (a>b && a>c) {
+//         printf("a oldest");
+//     } else if()
+// } hold
+
+// 25. A student will not be allowed to sit in an exam if his/her attendance is less than 75%.
+// Take following input from the user. Number of classes held. Number of classes attended. 
+// And print, percentage of class attended. Is the student is allowed to sit in the exam or not.
+
+// I am skip this question.
+
+// 26. Name the keyword which helps in writing code involves condition.
+
+// if elif else.
+
+// 27. Write the syntax of simple if statement.
+
+// I am skip this question.
+
+// 28. Is there any limit of statement that can appear under an if block.
+
+// I am skip this question.
+
+// 29. Write a program to check whether a person is eligible for voting or not. 
+// (accept age from user)
+
+// I am skip this question.
+
+// 30. Write a program to check whether a number is divisible by 7 or not.
+
+// I am skip this question.
+
+// 31. Write a program to display "Hello" if a number entered by user is a multiple of five ,
+//  otherwise print "Bye"
+
+// I am skip this question.
+
+// 32. Write a program to calculate the electricity bill (accept number of unit from user) 
+// according to the following criteria :
+//     Unit                                                        Price  
+// First 100 units                                               no charge
+// Next 100 units                                              Rs 5 per unit
+// After 200 units                                             Rs 10 per unit
+// (For example if input unit is 350 than total bill amount is Rs2000)
+
+// I am skip this question.
+
+// 33. Write a program to display the last digit of a number.
+
+// int main() {
+//     int n;
+//     printf("enter n");
+//     scanf("%d",&n);
+//     printf("%d",n%10);
+//     return 0;
+// }
+
+// 34. Write a program to check whether the last digit of a number( entered by user ) 
+// is divisible by 3 or not.
+
+// int main() {
+//     int n;
+//     printf("enter n");
+//     scanf("%d",&n);
+//     int r=n%10;
+//     if (r%3==0) {
+//         printf("yes");
+//     } else {
+//         printf("no");
+//     }
+//     return 0;
+// }
+
+// 35. Write a program to accept the cost price of a bike and display the road tax 
+// to be paid according to the following criteria : 
+        // Cost price (in Rs)                                         Tax
+        // > 100000                                                  15 %
+        // > 50000 and <= 100000                                     10%
+        // <= 50000                                                  5%
+
+// I am skip this question.
+
+// 36. Write the logical expression for the following: A is greater than B and 
+// C is greater than D
+
+// >
+
+// 37. Accept any city from the user and display the monument of that city.
+                //   City                                 Monument
+                //   Delhi                               Red Fort
+                //   Agra                                Taj Mahal
+                //   Jaipur                              Jal Mahal
+
+// int main() {
+//     char s[100];
+//     printf("enter s");
+//     scanf("%s",&s);
+//     if(s=='delhi'){
+//         printf("%s","red fort");
+//     } else if(s=='agra'){
+//         printf("%s","taj mahal");
+//     } else{
+//         printf("%s","jal mahal");
+//     }
+//     return 0;
+// }
+
+// 38. Write a program to check whether a person is a senior citizen or not.
+
+// Skip.
+
+// 39. Write a program to find the lowest number out of two numbers excepted from the user.
+
+// skip.
+
+// 40. Write a program to check whether a number entered is a three digit number or not.
+
+// int main() {
+//     int n;
+//     printf("enter n");
+//     scanf("%i",&n);
+//     if(n>99 && n<=1000) {
+//         printf("three digit");
+//     }else {
+//         printf("not");
+//     }
+//     return 0;
+// }
+
+// 41. Accept the temperature in degree Celsius of water and check whether it is boiling 
+// or not (boiling point of water in 100 oC.
+
+// skip.
+
+// 42. Write a program to accept two numbers and mathematical operators and perform 
+// operation accordingly Like:
+// Enter First Number: 7
+// Enter Second Number : 9
+// Enter operator : +
+// Your Answer is : 16
+
+// int main() {
+//     int n,m;
+//     printf("enter n m ");
+//     scanf("%d %d",&n,&m);
+//     char o;
+//     printf("enter o");
+//     scanf("%c",&o);
+//     if (o=='+') {
+//         // int a=n+m;
+//         printf("%d",n+m);
+//     } else if (o=='-') {
+//         int d=n-m;
+//         printf("%d",d);
+//     } else if (o=='*') {
+//         int p=n*m;
+//         printf("%d",p);
+//     } else if (o=='/') {
+//         int d=n/m;
+//         printf("%d",d);
+//     }
+//     return 0;
+// }
+
+// 44. Accept three numbers from the user and display the second largest number.
+
+// int main() {
+//     int a,b,c;
+//     printf("enter a b c");
+//     scanf("%d %d %d",&a,&b,&c);
+//     if ((a>b && a>c)&&(b<a && b>c)){
+//         printf("b is second largest");
+//     } else if((b>a && b>c)&&(a<b && a>c)){
+//         printf("a is second");
+//     } else {
+//         printf("c is second");
+//     }
+//     return 0;
+// }
+
+// 45. Accept the number of days from the user and calculate the charge 
+// for library according to following :
+// First five days : Rs 2/day.
+// Six to ten days  : Rs 3/day.
+// Ten to 15 days  : Rs 4/day
+// After 15 days    : Rs 5/day
+
+// skip
+
+// 46. Write a Python program to find those numbers which are divisible by 7 
+// and multiple of 5, between 1500 and 2700 (both included).
+
+// int main() {
+//     int a =1500;
+//     int b =2700;
+//     while (a<=b) {
+//         if (a%7==0 && a%5==0) {
+//             printf("%d\n",a);
+//         }
+//         a+=1;
+//     }
+//     return 0;
+// }
+
+// 47. Write a Python program to guess a number between 1 to 9
+
+// 48. Write a Python program to sum of two given integers. However, if the sum is between 15 
+// to 20 it will return 20.
+
+// int main() {
+//     int a,b,sum;
+//     printf("enter a b");
+//     scanf("%d %d",&a,&b);
+//     sum=a+b;
+//     if (sum>15 && sum<21) {
+//         printf("20");
+//     } else {
+//         printf("%d",sum);
+//     }
+//     return 0;
+// }
+
+// 49. Write a Python program to check a string represent an integer or not
+
+// skip string.
+
+// 50. Write a Python program that reads two integers representing a month and day 
+// and prints the season for that month and day.
+
+// string.
+
+// 51. Write a Python program to find the median of three values.
+
+// int main() {
+//     int a,b,c;
+//     printf("enter a b c");
+//     scanf("%d %d %d",&a,&b,&c);
+//     if ((a>b && a>c) && (b<a && b>c)){
+//         printf("%d",b);
+//     } else if ((b>a && b>c) && (c<b && c>a)){
+//         printf("%d",c);
+//     } else {
+//         printf("%d",a);
+//     }
+//     return 0;
+// }
+
+// 52. Write a Python program to get next day of a given date.
+
+// skip.
+
+// 53. Write a Python program to create the multiplication table (from 1 to 10) of a number.
+
+// Skip
+
+// 54. Write a Python program to calculate the length of a string.
+
+// 55. Write a Python program to add 'ing' at the end of a given string 
+// (length should be at least 3). If the given string already ends with 'ing' then add 'ly'
+//  instead. If the string length of the given string is less than 3, leave it unchanged
+
+// 56. Write a Python program to change a given string to a new string where the first 
+// and last chars have been exchanged.
+
+// 57. Write a Python program to display the current date and time.
+
+// 58. Write a Python program to display the current date and time.
+
+// 59. Write a Python program which accepts the radius of a circle from the user 
+// and compute the area.
+
+// int main() {
+//     int r;
+//     printf("enter radius");
+//     scanf("%d",&r);
+//     float p=22/7;
+//     float radius=r*r;
+//     float area=radius*p;
+//     printf("%f",area);
+//     return 0;
+// }
+
+// 60. Write a Python program which accepts the user's first and last name and print them
+//  in reverse order with a space between them.
+
+// string.
+
+// 61.Write a Python program to accept a filename from the user and print the extension of that. 
+
+// string.
+
+// 62. Write a Python program to test whether a number is within 100 of 1000 or 2000
+
+// 63. Write a Python program to get a new string from a given string where "Is" has
+//  been added to the front. If the given string already begins with "Is" then return 
+// the string unchanged.
+
+
---
+++ c/loop.c
@@ -0,0 +1,179 @@
+#include <stdio.h>
+
+// 1. Write a program to print the following using while loop.
+
+// A. First 10 Even Number.
+
+// int main() {
+//     int i=1;
+//     int a=2;
+//     while (i<=10) {
+//         printf("%d\n",a);
+//         a+=2;
+//         i+=1;
+//     }
+//     return 0;
+// }
+
+// B. First 10 natural number.
+
+// int main() {
+//     int i=1;
+//     while (i<=10) {
+//         printf("%d\n",i);
+//         i++;
+//     }
+// }
+
+// C. First 10 odd numbers.
+
+// int main() {
+//     int i=1;
+//     int a=1;
+//     while (i<=10){
+//         printf("%d\n",a);
+//         a+=2;
+//         i++;
+//     }
+// }
+
+// D. First 10 whole numbers.
+
+// int main() {
+//     int i=0;
+//     while (i<10){
+//         printf("%d\n",i);
+//         i++;
+//     }
+// }
+
+// 2. Write a program to print first 20 integers and their squares be like.
+
+// int main() {
+//     int i=1;
+//     while (i<=20) {
+//         int a=i*i;
+//         printf("%d %d \n",i,a);
+//         i+=1;
+//     }
+// }
+
+// 3. Write while loop statement to print the following series.
+
+// int main() {
+//     int a=10;
+//     while (a<=300){
+//         printf("%d\n",a);
+//         a+=10;
+//     }
+//     return 0;
+// }
+
+// 4. Write a while loop statement to print the following series.
+
+// int main(){
+//     int a=105;
+//     while (a>0){
+//         printf("%d\n",a);
+//         a-=7;
+//     }
+//     return 0;
+// }
+
+// 5. Write a program to print the first 10 natural number in reverse order.
+
+// int main() {
+//     int a=10;
+//     while (a>0){
+//         printf("%d\n",a);
+//         a-=1;
+//     }
+//     return 0;
+// }
+
+// 6. write a program to print the sum of the first 10 natural number.
+
+// int main() {
+//     int i=1;
+//     int sum=0;
+//     while(i<=10){
+//         sum=sum+i;
+//         i=i+1;
+//     }
+//     printf("%d",sum);
+//     return 0;
+// }
+
+// 7. Write a program to print the sum of the first 10 Even numbers.
+
+// int main() {
+//     int i=1;
+//     int sum=0;
+//     int a=2;
+//     while (i<=10){
+//         sum+=a;
+//         a+=2;
+//         i+=1;
+//     }
+//     printf("%d",sum);
+//     return 0;
+// }
+
+// 8. Write a program to print a table of a number entered from the user.
+
+// int main() {
+//     int i=1,a;
+//     printf("enter user number");
+//     scanf("%d",&a);
+//     while(i<=10){
+//         printf("%d * %d = %d\n",a,i,a*i);
+//         i+=1;
+//     }
+//     return 0;
+// }
+
+// 9. Write a program to display all even numbers that fall between two numbers 
+// (exclusive both numbers) entered by the user.
+
+// int main(){
+//     int a,b;
+//     printf("enter a b");
+//     scanf("%d %d",&a,&b);
+//     while(a<=b){
+//         if (a%2==0){
+//             printf("%d\n",a);
+//         }
+//         a+=2;
+//     }
+//     return 0;
+// }
+
+// 10. Write a program to check whether a number is prime or not.
+
+// int main() {
+//     int i=1,a,count=0;
+//     printf("enter a");
+//     scanf("%d",&a);
+//     while(i<=a){
+//         if (a%i==0){
+//             count+=1;
+//         }
+//         i+=1;
+//     }
+//     if (count==2){
+//         printf("yes");
+//     }else {
+//         printf("no");
+//     }
+//     return 0;
+// }
+
+// 11. Write a program to find the sum of the digits of a number accepted from the user.
+
+int main() {
+    int i=0,a;
+    printf("enter a");
+    scanf("%d",&a);
+    
+}
+
